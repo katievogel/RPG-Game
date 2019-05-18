4bb8424a1939a5d0609f97973fca9a447094588d
Katie Vogel <kvog983@gmail.com>
2019-05-18 19:21:02 -0400

    styling updated

--- Assets/game.js
+++ Assets/game.js
@@ -1,10 +1,10 @@
-//TODO: Assign each crystal a random value from of 1-12 that is     hidden to the player
+//TODO: Assign each crystal a random value from of 1-12 that is hidden to the player
 var blueGem = Math.floor(Math.random() * 12) + 1;
 var purpleGem = Math.floor(Math.random() * 12) + 1;
 var greenGem = Math.floor(Math.random() * 12) + 1;
 var redGem = Math.floor(Math.random() * 12) + 1;
 
-//TODO: Assign a random opponent number to compare against,         from 19-120. Don't forget to create and call the function       for it to show up on the page
+//TODO: Assign a random opponent number to compare against, from 19-120. Don't forget to create and call the function for it to show up on the page
 var opponentNum = Math.floor(Math.random() * 101) + 19;
 function newOppNum() {
     $('#random-num').html(opponentNum);
@@ -18,7 +18,7 @@ var playerTotal = 0;
 //code below is so it starts at zero on page load instead of being blank
 $('.total-score').html(playerTotal); 
 
-//TODO: On button/gem click the number assigned should add to       the player's total score. The player can continue to add/       click gems until they match or go over the opponent number.
+//TODO: On button/gem click the number assigned should add to the player's total score. The player can continue to add/click gems until they match or go over the opponent number.
 
 $('button.blue').click(function gemSum() {
     playerTotal = blueGem + playerTotal;
@@ -48,7 +48,7 @@ $('button.red').click(function gemSum() {
     lossesIncr();
 });
 
-//TODO: Wins: the player must match the opponent number. Each       game won must be added to 'wins' 
+//TODO: Wins: the player must match the opponent number. Each game won must be added to 'wins' 
 function winsIncr() {
     if (playerTotal === opponentNum) {
         $('.wins').html(++wins);
@@ -56,7 +56,7 @@ function winsIncr() {
     }
 };
 
-//TODO: Losses: if the player goes over the opponent number         they lose. Each loss must be added to the 'losses'
+//TODO: Losses: if the player goes over the opponent number they lose. Each loss must be added to the 'losses'
 function lossesIncr() {
     if (playerTotal > opponentNum) {
         $('.losses').html(++losses);
@@ -64,7 +64,7 @@ function lossesIncr() {
     }
 };
 
-//TODO: Need a reset to assign new numbers to the gems for each     new game. Page refresh will not work as that will clear the     wins and losses
+//TODO: Need a reset to assign new numbers to the gems for each new game. Page refresh will not work as that will clear the wins and losses
 function newGame (){
     blueGem = Math.floor(Math.random() * 12) + 1;
     purpleGem = Math.floor(Math.random() * 12) + 1;
